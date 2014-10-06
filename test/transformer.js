0813820536056a5d6239067a927282f787500d11
filipovsky <filipovskii.off@gmail.com>
2014-10-06 15:51:42 +0400

    Move transformers to separate folder.

--- test/transformer.js
+++ test/transformer.js
@@ -1,81 +1,15 @@
 'use strict';
 
 var recast = require('recast'),
-    b = require('recast').types.builders,
     debug = require('debug')('looks:transformer'),
     assert = require('assert'),
+    GatherDeclarations = require('../lib/transformers/GatherDeclarations'),
+    SortDeclarationsByName = require('../lib/transformers/SortDeclarationsByName'),
+    SortDeclarationsByInit = require('../lib/transformers/SortDeclarationsByInit'),
     _ = require('lodash');
 
 
 describe('VarTransformer', function() {
-
-  var GatherDeclarations = recast.Visitor.extend({
-    visitFunctionDeclaration: function(stmt) {
-      var body = stmt.body,
-          newBody,
-          declarations,
-          newDeclarators,
-          newDeclaration;
-
-      if (body.type && body.type === 'BlockStatement') {
-        body = body.body;
-      }
-
-      newBody = _.filter(body, function(node) {
-        return node.type !== 'VariableDeclaration';
-      });
-
-      declarations = _.filter(body, function(node) {
-        return node.type === 'VariableDeclaration';
-      });
-
-      body.length = 0;
-
-      newDeclarators = _.chain(declarations)
-        .pluck('declarations')
-        .flatten()
-        .map(function(d) {
-          return b.variableDeclarator(d.id, d.init);
-        })
-        .sortBy(function(decl) {
-          return +(decl.init === null);
-        })
-        .value();
-
-      newDeclaration = b.variableDeclaration('var', newDeclarators);
-
-      body.push(newDeclaration);
-      _.forEach(newBody, function(part) {
-        body.push(part);
-      });
-
-      return stmt;
-    }
-  });
-
-
-  var SortDeclarationsByName = recast.Visitor.extend({
-
-    visitVariableDeclaration: function(node) {
-      var newDeclarators;
-      newDeclarators = _.sortBy(node.declarations, function(decl) {
-        return decl.id.name;
-      });
-      return b.variableDeclaration('var', newDeclarators);
-    }
-  });
-
-
-  var SortDeclarationsByInit = recast.Visitor.extend({
-    visitVariableDeclaration: function(node) {
-      var newDeclarators;
-      newDeclarators = _.sortBy(node.declarations, function(decl) {
-        return +(decl.init === null);
-      });
-      return b.variableDeclaration('var', newDeclarators);
-    }
-  });
-
 
   it('can fix declarations', function() {
     var gather = new GatherDeclarations(),
